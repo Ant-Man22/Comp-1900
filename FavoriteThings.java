3197819cce77944b269099b70eba114ff637c783
Anthony  Elmore <43595675+Ant-Man22@users.noreply.github.com>
2018-12-03 13:57:06 -0600

    Add files via upload

---
+++ FavoriteThings.java
@@ -0,0 +1,10 @@
+
+
+public class FavoriteThings{
+    public static void main(String[] args){
+        System.out.println("My Favorite Video Games");
+        System.out.println("1. Call Of Duty");
+        System.out.println("2. Grand Theft Auto");
+       System.out.println("3. Batman");
+    }
+}
---
+++ MyFirstGame.java
@@ -0,0 +1,12 @@
+
+/*
+A very simple fighting game!
+*/
+
+public class MyFirstGame {
+    public static void main(String[] args){
+        int enemyHP = 100;
+        System.out.println("Current enemy HP: " + enemyHP);
+    }
+}
+
